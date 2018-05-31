e30adcdf1429264779dc84ae71505d78b7fd3069
Yana Ustsinovich <yustinovich@icloud.com>
2018-05-31 23:08:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,5 @@
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  
 end
 
 
@@ -11,7 +12,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
